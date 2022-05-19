9d52fab36c486b761f7328949d1c75cfffff99f9
Николай Клековкин <nickolai.klekovkin@yandex.ru>
2022-05-19 13:02:21 +0500

    11

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,9 +5,7 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(
-        max_length=200
-        )
+    title = models.CharField(max_length=200)
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор')
